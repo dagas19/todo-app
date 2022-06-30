b2e0923df1874ccbcaa42d1a6ef1d2def47c5240
dagas19 <dagas199@gmail.com>
2022-07-01 06:17:03 +0800

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
